fb6b43a0868fb6817df84008cfd6116f6583e999
Stefano Del Furia <stefano.delfuria@franchettisalviani.net>
2026-04-08 20:36:55 +0200

    Project Placeholder

---
+++ TaaS_RestServer/src/taas_restserver/TaaS_RestServer.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package taas_restserver;
+
+/**
+ *
+ * @author delfo
+ */
+public class TaaS_RestServer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
